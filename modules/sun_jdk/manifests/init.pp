e6127d730753446c289bb55cca2f27db1cde27fd
Jim Moore <jim.moore@canoe-ventures.com>
2012-02-06 23:49:07 -0700

    Adds the 'unlimited' Java Cryptography policy files.

--- modules/sun_jdk/manifests/init.pp
+++ modules/sun_jdk/manifests/init.pp
@@ -26,6 +26,12 @@ class sun_jdk(
     include 'sun_jdk::params'
   }
 
+  # where to find files; first try local filesystem, then the puppet master
+  $module = 'sun_jdk'
+  $prefix = '/etc/puppet/modules'
+  $p1     = "${prefix}/${module}/files"
+  $p2     = "puppet://modules/${module}"
+
   $the_package_name = $jdk_package_name ? {
     'UNSET' => $sun_jdk::params::jdk_package_name, default => $jdk_package_name}
   $the_version = $jdk_version ? {
@@ -35,6 +41,22 @@ class sun_jdk(
 
   package { $the_package_name:
     ensure => "${the_version}-fcs",
+  }
+
+  # Install the "unlimited" Java Cryptography policy files
+  File {
+    mode    => '644',
+    owner   => 'root',
+    group   => 'root',
+    require => Package[$the_package_name],
+  }
+  file { 'unlimited_local_policy.jar':
+    path    => "${java_home}/jre/lib/security/local_policy.jar",
+    source  => ["${p1}/local_policy.jar", "${p2}/local_policy.jar"],
+  }
+  file { 'unlimited_US_export_policy.jar':
+    path    => "${java_home}/jre/lib/security/US_export_policy.jar",
+    source  => ["${p1}/US_export_policy.jar", "${p2}/US_export_policy.jar"],
   }
 
 }
